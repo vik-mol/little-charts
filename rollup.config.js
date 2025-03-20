a996824aa30888306969e090492caba3f00c86a5
Mol Victor <molvv@bcs.ru>
2025-03-20 22:18:46 +0400

    chore: initial

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  output: [{ file: "dist/index.esm.js", format: "esm" }],
+  plugins: [typescript()],
+  external: [],
+};
